cf4dc591386528bd622c000e23f18e7f6d3f14f0
Nep <56213204+TheNepper@users.noreply.github.com>
2023-10-21 17:38:24 -0700

    login functionality

--- application/backend/routes/users.js
+++ application/backend/routes/users.js
@@ -43,5 +43,46 @@ router.post("/PostUsers", async (request, response) => {
     }
 })
 
+router.post('/login',async (request, response) => {
+    
+    const email = request.body.email.toLowerCase(); //uname will be email
+    const password = request.body.password;
+    console.log(email);
+    //query to return the row with correct email and pass from db
+    const query = `SELECT COUNT(*) as count FROM FoodOrderSys.LoginDetails WHERE email = '${email}' AND password = '${password}'`
+    const update = `UPDATE FoodOrderSys.LoginDetails SET loggedIn = 1 WHERE email = '${email}'`
+
+        try
+        {    
+            const validation = await db.promise().query(query)
+            const emailExists = validation[0][0].count
+            console.log(emailExists);
+            if(emailExists > 0) //change to validation
+            {
+                // query to update the login status of a user 
+                await db.promise().query(update);
+                console.log("logged in successfull");
+                return response.status(200).json({message: "Login successful"});
+                response.redirect('/menu');
+            }
+            else
+            {
+                return response.status(200).json({message: "Either Email or Password is invalid"});
+            }
+
+        }
+        catch (error)
+        {
+            response.status(500).json({ error: "Internal Server Error" });
+        }            
+
+    
+});  
+/*
+logout:
+
+router.post('/logout')
+*/
+
 
 module.exports = router;
