7454a673d1e8bc8dab7b7b5561ac846f7afee3f0
Nep <56213204+TheNepper@users.noreply.github.com>
2023-10-25 23:38:56 -0700

    added login for admin but need to format the frontend to take in the functions

--- application/backend/routes/admin.js
+++ application/backend/routes/admin.js
@@ -18,4 +18,41 @@ router.get('/GetUsers', async (request, response) => {
     response.status(200).send(results[0]);
 });
 
+router.post('/login',async (request, response) => {
+    
+    const email = request.body.email.toLowerCase(); 
+    const password = request.body.password;
+
+    //query to return the row with correct email and pass from db
+    const query = `SELECT *, COUNT(*) as count FROM FoodOrderSys.adminDetails WHERE email = '${email}' AND password = '${password}'`;
+
+        try
+        {    
+            const validation = await db.promise().query(query);
+            const emailExists = validation[0][0].count;
+
+            if(emailExists > 0) 
+            {
+                // query to update the login status of a user 
+                await db.promise().query(update);
+
+                //cookie-token
+                const username = validation[0][0].username;
+                const userID = validation[0][0].userID;
+                console.log("works");
+                return response.status(200).json({message: "Login successful"});
+            }
+            else
+            {
+                return response.status(200).json({Failmessage: "Either Email or Password is invalid"});
+            }
+
+        }
+        catch (error)
+        {
+            console.log(error);
+            response.status(500).json({ error: "Internal Server Error" });
+        }            
+
+    });
 module.exports = router;
