818d75878998cd5192d24f5bc450072077d51768
Sutaria01 <maliekasutaria@gmail.com>
2023-11-08 13:38:23 -0800

    Menu Filters

--- application/pizza/src/Pages/AdminLogin.js
+++ application/pizza/src/Pages/AdminLogin.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import BannerImage from '../Assests/pizza.jpeg';
 import '../styles/Login.css';
 import { useNavigate } from 'react-router-dom';
 
