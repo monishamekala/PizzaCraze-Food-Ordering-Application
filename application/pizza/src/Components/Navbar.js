adc6493de687466a104a13cfcf2edb442fafe21e
Monisha Mekala <monishasmekala@gmail.com>
2023-11-07 16:02:22 -0800

    Align the card in the menu

--- application/pizza/src/Components/Navbar.js
+++ application/pizza/src/Components/Navbar.js
@@ -74,10 +74,13 @@ function Navbar() {
             </Link>
 
           </div>
+
           <div className='rightbuttons'>
             {auth ? <Link to={`/mycart/${userID}`}><img src={cartlogo} alt={username}/></Link> : <Link to= '/login'> <img onClick = {AskToLogin} src={cartlogo} alt='profile'/></Link> }
-
-            {auth ? <Link to={`/profile/${userID}`}><img src={userlogo} alt={username}/></Link> : <Link to="/login"> <img src={loginlogo} alt='profile'/></Link> }
+          </div>
+          
+          <div className='rightbuttons'>
+          {auth ? <Link to={`/profile/${userID}`}><img src={userlogo} alt={username}/></Link> : <Link to="/login"> <img src={loginlogo} alt='profile'/></Link> }
           </div>
        
         </div>
