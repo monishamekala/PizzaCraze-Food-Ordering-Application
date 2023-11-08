493f27b83d2ecaab0618ee42e24351235bff4d5c
Sutaria01 <maliekasutaria@gmail.com>
2023-11-08 13:46:54 -0800

    update css changes

--- application/pizza/src/Pages/Menu.js
+++ application/pizza/src/Pages/Menu.js
@@ -1,59 +1,87 @@
-// import React from 'react'
-import Menuitem from './Menuitem'
-import '../styles/menuitem.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import Menuitem from './Menuitem';
+import '../styles/MenuFilter.css';
 
 function Menu() {
-  const [menuItems, setmenu] = useState([]);
-
+  const [menuItems, setMenu] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('All'); // Initial category is 'All'
   const navigate = useNavigate();
 
-  useEffect( () => {
-    const fetchMentuDetails = async () => {
-      try{
+  useEffect(() => {
+    const fetchMenuDetails = async () => {
+      try {
         const urlMenu = "/api/MenuController/GetMenu";
         const res = await axios.get(process.env.REACT_APP_API_URL.concat(urlMenu));
-        setmenu(res.data);
-      }catch(err){
+        setMenu(res.data);
+      } catch (err) {
         console.log(err);
       }
     }
-    fetchMentuDetails()
-  }, [])
+    fetchMenuDetails()
+  }, []);
 
   const CheckLogin = async () => {
-    try{
+    try {
       const urlCurrentUser = "/api/UserController/CurrentUser";
-      const response = await axios.get(process.env.REACT_APP_API_URL.concat(urlCurrentUser), {withCredentials: true});
+      const response = await axios.get(process.env.REACT_APP_API_URL.concat(urlCurrentUser), { withCredentials: true });
 
-      if(response.data.Status === "Success"){
+      if (response.data.Status === "Success") {
         const user = response.data.userID;
         const url = `/mycart/${user}`;
         navigate(url);
       }
-      else{
+      else {
         alert("Please log in");
       }
-    }catch(err){
+    } catch (err) {
       console.log(err);
     }
   }
+
+  const categories = [...new Set(menuItems.map(item => item.category))];
+
+  // Filter menu items based on the selected category
+  const filteredMenuItems = selectedCategory === 'All'
+    ? menuItems
+    : menuItems.filter(item => item.category === selectedCategory);
+
   return (
     <div>
-      <div className='container my-4' >
-        <div className='row'>
-          {menuItems.map(eachItem => (
-          <div className='col-md-4' key={eachItem.menu_id} >
-            <Menuitem itemID = {eachItem.menu_id} name = {eachItem.name} price = {eachItem.price} image_url = {eachItem.image_url} description = {eachItem.description}></Menuitem>
-          </div> 
+      <div className='container my-4'>
+        <div>
+          <button className='custom-button-filter' onClick={() => setSelectedCategory('All')}>All</button>
+          {categories.map(category => (
+            <button className='custom-button-filter' key={category} onClick={() => setSelectedCategory(category)}>{category}</button>
           ))}
         </div>
+
+        {categories.map(category => (
+          <div key={category}>
+            <h2>{category}</h2>
+            <div className='row'>
+              {filteredMenuItems
+                .filter(item => item.category === category)
+                .map(eachItem => (
+                  <div className='col-md-4' key={eachItem.menu_id}>
+                    <Menuitem
+                      itemID={eachItem.menu_id}
+                      name={eachItem.name}
+                      price={eachItem.price}
+                      image_url={eachItem.image_url}
+                      description={eachItem.description}
+                      category={eachItem.category}
+                    />
+                  </div>
+                ))}
+            </div>
+          </div>
+        ))}
       </div>
       <button className="btn btn-primary custom-button" onClick={CheckLogin}>Go to Cart</button>
     </div>
   )
 }
 
-export default Menu
+export default Menu;
