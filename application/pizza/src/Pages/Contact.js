4e912ae63db315eee558ac1f82b191ed4020bd52
Joey Palanca <jpalanca@sfsu.edu>
2023-11-13 15:11:50 -0800

    Centered FAQ, might need to polish later

--- application/pizza/src/Pages/Contact.js
+++ application/pizza/src/Pages/Contact.js
@@ -9,36 +9,35 @@ function Contact() {
         <div className='my-3 aboutBottom'>
             <h1>Contact US</h1>
             <div className="my-5 d-flex justify-content-between">
-            <Link to={'https://instagram.com'}><button type="button" className="btn btn-dark">Instagram</button></Link>
-        <Link to={'mailto:pizzacrazesf@gmail.com'}>
-        <button type="button" className="btn btn-dark">Mail</button></Link>
-        <Link to={'https://twitter.com'}>
-        <button type="button" className="btn btn-dark">Twitter</button></Link>
-        
-
+              <Link to={'https://instagram.com'}>
+            <button type="button" className="btn btn-dark">Instagram</button></Link>
+            <Link to={'mailto:pizzacrazesf@gmail.com'}>
+            <button type="button" className="btn btn-dark">Mail</button></Link>
+            <Link to={'https://twitter.com'}>
+            <button type="button" className="btn btn-dark">Twitter</button></Link>
             </div>
-            <br></br>
             <div className="my-3 d-flex justify-content-around">
-              <h1>FAQ</h1>
-              What types of pizza do you offer?
-              Can I customize my own pizza with specific toppings?
-              Do you offer gluten-free crust options?What sizes of pizzas are available?
-              Is there a menu for sides and beverages?
-              Do you have vegetarian or vegan pizza options?
-              What are your delivery areas and charges?
-              How can I place an order online?
-              What are your business hours?
-              Are there any current promotions or discounts?
-              Do you offer catering services for events?
-              What payment methods do you accept?
-              How do I track my delivery order?
-              Can I order in advance or schedule a future delivery?
-              What is your policy on returns or refunds?
-              Are nutritional information and allergen details available for your menu items?
-              Do you offer contactless delivery or pickup options?
-              Can I join a loyalty or rewards program?
-              Are there any special deals for large group orders?
-              How do I contact customer support for any issues with my order?
+              FAQ<br></br>
+              What types of pizza do you offer?<br></br>
+              Can I customize my own pizza with specific toppings?<br></br>
+              Do you offer gluten-free crust options?<br></br>
+              What sizes of pizzas are available?<br></br>
+              Is there a menu for sides and beverages?<br></br>
+              Do you have vegetarian or vegan pizza options?<br></br>
+              What are your delivery areas and charges?<br></br>
+              How can I place an order online?<br></br>
+              What are your business hours?<br></br>
+              Are there any current promotions or discounts?<br></br>
+              Do you offer catering services for events?<br></br>
+              What payment methods do you accept?<br></br>
+              How do I track my delivery order?<br></br>
+              Can I order in advance or schedule a future delivery?<br></br>
+              What is your policy on returns or refunds?<br></br>
+              Are nutritional information and allergen details available for your menu items?<br></br>
+              Do you offer contactless delivery or pickup options?<br></br>
+              Can I join a loyalty or rewards program?<br></br>
+              Are there any special deals for large group orders?<br></br>
+              How do I contact customer support for any issues with my order?<br></br>
             </div>
             
         </div>
